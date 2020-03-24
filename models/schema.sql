54762433d5e1552d44349a6d0b46b6c3b2669600
Ian Hooper <ianhooper14@gmail.com>
2020-03-23 22:00:58 -0400

    updated w/ Ian Code

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,13 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS travel_db;
+CREATE DATABASE travel_db;
+USE travel_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE travel
+(
+	id int NOT NULL AUTO_INCREMENT,
+	city varchar(300) NOT NULL,
+	flight varchar(300) NOT NULL,,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO travel (city, flight) VALUES ("Los Angeles, CA", "Jetblue");
