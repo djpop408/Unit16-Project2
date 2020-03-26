23821d7aade362362facb8bf4e099413f0fe76bd
Anthony Nguyen <anthonynguyen@Anthonys-MBP.attlocal.net>
2020-03-25 20:36:23 -0700

    change keys path

--- config/passport-config.js
+++ config/passport-config.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
-const keys = require("./keys.js");
+const keys = require("../keys.js");
 const db = require("../models");
 const Users = db.Users;
 
