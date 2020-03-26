15fa536ffb92e1f9ddd15ef5c53e214be99672b1
Anthony Nguyen <anthonynguyen@Anthonys-MBP.attlocal.net>
2020-03-25 20:56:17 -0700

    updating keys

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // google oauth
+    google:{
+        clientID:'313856255580-nhsbjui7csrbb5iafa6b97bjo8896j64.apps.googleusercontent.com',
+        clientSecret:'LsNksPwoF4oO-kv-68z5zMqE'
+    },
+    cookieSession:{
+        sessioinKey:"globetrotterCookieSessionKey"
+    }
+}
